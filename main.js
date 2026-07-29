fadbeb94b0faf5a72ddb7d2214db55f8090b7a1b
youssefmoham655-dev <youssefmoham655@gmail.com>
2026-07-29 21:43:06 +0300

    The randomization of choose of the computer was made

--- main.js
+++ main.js
@@ -13,6 +13,22 @@ function choose(userpick) {
     else if (userpick === 'scissors') {
         userimg.src = 'assets/Scissors.png';
     }
+
+// random display
+
+    const e5tyarat = ['assets/Paper.png', 'assets/rock.png', 'assets/Scissors.png']
+    const computerimg = document.getElementById('computer-img');
+
+    const randomizeloop = setInterval(() => {
+        const choice = Math.floor(Math.random() * e5tyarat.length);
+        computerimg.src = e5tyarat[choice];
+        computerimg.src = e5tyarat[randomIndex];
+    }, 150);
+
+    setTimeout(() => {
+        clearInterval(randomizeloop);
+    }, 3000);
+
 // disable
     const paper = document.getElementById("paper");
     const rock = document.getElementById("rock");
@@ -26,6 +42,7 @@ function choose(userpick) {
 
         scissors.style.pointerEvents = 'none';
         scissors.style.opacity = '0.5';
+        randomizeloop;
 
         setTimeout(function() {
             rock.style.pointerEvents = 'auto';
@@ -33,6 +50,7 @@ function choose(userpick) {
 
             scissors.style.pointerEvents = 'auto';
             scissors.style.opacity = '1';
+            letsStartText.textContent = "Let's start";
         }, 3000);
     }
 
@@ -44,12 +62,15 @@ function choose(userpick) {
         scissors.style.pointerEvents = 'none';
         scissors.style.opacity = '0.5';
 
+        randomizeloop;
+
         setTimeout(function() {
             paper.style.pointerEvents = 'auto';
             paper.style.opacity = '1';
 
             scissors.style.pointerEvents = 'auto';
             scissors.style.opacity = '1';
+            letsStartText.textContent = "Let's start";
         }, 3000);
     }
 
@@ -62,12 +83,15 @@ function choose(userpick) {
         paper.style.pointerEvents = 'none';
         paper.style.opacity = '0.5';
 
+        randomizeloop;
+
         setTimeout(function() {
             rock.style.pointerEvents = 'auto';
             rock.style.opacity = '1';
 
             paper.style.pointerEvents = 'auto';
             paper.style.opacity = '1';
+            letsStartText.textContent = "Let's start";
         }, 3000);
     }
     }
